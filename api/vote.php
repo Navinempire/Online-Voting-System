bd8da7a37f8dab97868b7bd1545053f3869a1d87
Navin <d.navinempire27@email.com>
2024-12-19 15:02:18 +0530

    dashboard.php file added

---
+++ api/vote.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include('connect.php');
+
+$votes = $_POST['gvotes'];
+$total_votes = $votes+1;
+$gid = $_POST['gid'];
+$uid =$_SESSION['userdata']['id'];
+
+$update_votes = mysqli_query($connect,"UPDATE user SET votes='$total_votes'WHERE id='$gid' ");
+$update_votes_status = mysqli_query($connect,"UPDATE user SET status=1 WHERE id='$uid' ");
+
+if($update_votes and $update_votes_status){
+    $groups = mysqli_query($connect,"SELECT * FROM user WHERE role=2");
+    $_SESSION['userdata']['status']=1;
+    $_SESSION['groupsdata']=$groupsdata;
+}
+else{
+    echo'
+    <scropt>
+    alert("Some error occured");
+    window.location = "../routes/deshboard.php";
+    </script>
+    ';
+}
+?>
