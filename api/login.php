d5479a109882c9aefb4eceeccdbfb70f00939831
Navin <d.navinempire27@email.com>
2024-12-20 10:52:04 +0530

    index.html file added

--- api/login.php
+++ api/login.php
@@ -6,20 +6,30 @@ $mobile = $_POST['mobile'];
 $password = $_POST['password'];
 $role = $_POST['role'];
 
+
 $check = mysqli_query($connect,"SELECT * FROM USER WHERE mobile='$mobile' AND password='$password' AND role='$role' ");
+
 if (mysqli_num_rows($check) > 0) {
+    
     $userdata = mysqli_fetch_array($check);
+    
     $groups = mysqli_query($connect,"SELECT * FROM user WHERE role=2");
+    
+    if (!$groups) {
+        die("Groups query failed: " . mysqli_error($connect));
+    }
     $groupsdata = mysqli_fetch_all($groups,MYSQLI_ASSOC);
+    
 
     $_SESSION['userdata'] = $userdata;
     $_SESSION['groupsdata'] = $groupsdata;
-
-    echo'
+    echo "its ok goog nice";
+    
+    echo '
     <script>
-    windows.location = "../routes/dashboard.php";
-    </script>
-    ';
+        window.location.href = "../routes/dashboard.php";
+    </script>';
+    
 }
 else {
     echo'
